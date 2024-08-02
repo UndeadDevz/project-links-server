d0d560ccf0cfbac40b2ab2602f07db715401ea86
quantumvflux <marcosabaravalle@gmail.com>
2024-08-02 14:19:25 -0300

    add image url to user media field in db

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { SharpPipe } from 'src/sharp/sharp.pipe';
+import { UserService } from 'src/user/user.service';
 
 @Controller('image')
 export class ImageController {
-    constructor(private readonly cloudinaryService: CloudinaryService) { }
+    constructor(
+        private readonly cloudinaryService: CloudinaryService,
+        private readonly userService: UserService,
+    ) { }
 
     @Post('upload')
+<<<<<<< Updated upstream
     @UseInterceptors(FileInterceptor('file'))
     uploadImage(@UploadedFile(SharpPipe) file: Express.Multer.File) {
         return this.cloudinaryService.uploadFile(file)
+=======
+    @UseInterceptors(FileInterceptor('image'))
+    async uploadImage(
+        @UploadedFile(SharpPipe) image: Express.Multer.File,
+        @Body('userId') userId: string,
+    ) {
+        const uploadResult = this.cloudinaryService.uploadFile(image)
+
+        await this.userService.addImageToMedia(userId, (await uploadResult).url)
+
+        return { url: (await uploadResult).url }
+>>>>>>> Stashed changes
     }
 }
