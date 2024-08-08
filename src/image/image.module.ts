fe81a32e38f0d537eafb58bffacc2356e94f1e94
Razgrizsx <razgrisss41@gmail.com>
2024-08-08 14:37:05 -0300

    module in image

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { ImageController } from './image.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-    imports: [UserModule],
-    providers: [CloudinaryService],
-    controllers: [ImageController],
+  imports: [UserModule],
+  providers: [CloudinaryService, JwtService],
+  controllers: [ImageController],
 })
-export class ImageModule { }
+export class ImageModule {}
