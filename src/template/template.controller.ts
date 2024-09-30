cc64924cebc07fb4e685336e9626613766e1f085
quantumvflux <marcosabaravalle@gmail.com>
2024-09-30 16:47:50 -0300

    fix(templates): fix getUserTemplate function

--- src/template/template.controller.ts
+++ src/template/template.controller.ts
@@ -4,15 +4,18 @@ import { Prisma, Template } from '@prisma/client';
 
 @Controller('template')
 export class TemplateController {
-    constructor(private readonly templateService: TemplateService) { }
+  constructor(private readonly templateService: TemplateService) {}
 
-    @Get(":user_id")
-    getUserTemplates(@Param() user_id: string): Promise<Template[]> {
-        return this.templateService.getUserTemplates(user_id)
-    }
+  @Get(':user_id')
+  getUserTemplates(@Param('user_id') user_id: string): Promise<Template[]> {
+    return this.templateService.getUserTemplates(user_id);
+  }
 
-    @Post(":user_id")
-    createTemplate(@Param("user_id") user_id, @Body() data: Prisma.TemplateCreateInput): Promise<Template> {
-        return this.templateService.createTemplate(user_id, data)
-    }
+  @Post(':user_id')
+  createTemplate(
+    @Param('user_id') user_id,
+    @Body() data: Prisma.TemplateCreateInput,
+  ): Promise<Template> {
+    return this.templateService.createTemplate(user_id, data);
+  }
 }
--- src/template/template.service.ts
+++ src/template/template.service.ts
@@ -4,28 +4,29 @@ import { Prisma, Template } from '@prisma/client';
 
 @Injectable()
 export class TemplateService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async getUserTemplates(user_id: string): Promise<Template[]> {
-        return this.prisma.template.findMany({
-            where: {
-                owner: {
-                    user_id: user_id
-                }
-            }
-        })
-    }
+  async getUserTemplates(user_id: string): Promise<Template[]> {
+    return this.prisma.template.findMany({
+      where: {
+        owner_id: user_id,
+      },
+    });
+  }
 
-    async createTemplate(user_id: string, data: Prisma.TemplateCreateInput): Promise<Template> {
-        return this.prisma.template.create({
-            data: {
-                ...data,
-                owner: {
-                    connect: {
-                        user_id: user_id
-                    }
-                }
-            }
-        });
-    }
+  async createTemplate(
+    user_id: string,
+    data: Prisma.TemplateCreateInput,
+  ): Promise<Template> {
+    return this.prisma.template.create({
+      data: {
+        ...data,
+        owner: {
+          connect: {
+            user_id: user_id,
+          },
+        },
+      },
+    });
+  }
 }
