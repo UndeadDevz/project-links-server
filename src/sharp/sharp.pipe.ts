f4b9045c6170523dea6b2535a8da8c5cf02c7a32
quantumvflux <marcosabaravalle@gmail.com>
2024-10-03 13:45:56 -0300

    fix(sharp): quitar villereada del danel

--- src/sharp/sharp.pipe.ts
+++ src/sharp/sharp.pipe.ts
@@ -1,19 +1,20 @@
-/* eslint-disable prettier/prettier */
-import { Injectable, PipeTransform } from "@nestjs/common";
-import * as path from "path"
-import * as sharp from "sharp"
+import { Injectable, PipeTransform } from '@nestjs/common';
+import * as path from 'path';
+import * as sharp from 'sharp';
 
 @Injectable()
-export class SharpPipe implements PipeTransform<Express.Multer.File, Promise<any>> {
-    async transform(image: Express.Multer.File): Promise<any> {
-        const originalName = path.parse(image.originalname).name;
-        const filename = Date.now() + "-" + originalName + '.webp'
+export class SharpPipe
+  implements PipeTransform<Express.Multer.File, Promise<any>>
+{
+  async transform(image: Express.Multer.File): Promise<any> {
+    const originalName = path.parse(image.originalname).name;
+    const filename = Date.now() + '-' + originalName + '.webp';
 
-        const buffer = await sharp(image.buffer)
-            .resize(450)
-            .webp({ effort: 3 })
-            .toBuffer()
+    const buffer = await sharp(image.buffer)
+      .resize(450)
+      .webp({ effort: 3 })
+      .toBuffer();
 
-        return { buffer, filename }
-    }
+    return { buffer, filename };
+  }
 }
