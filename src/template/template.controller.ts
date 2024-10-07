efbb5460b03c94dff26a0470d15421d7589d37c5
Razgrizsx <razgrisss41@gmail.com>
2024-10-07 14:34:51 -0300

    create

--- src/template/template.controller.ts
+++ src/template/template.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { TemplateService } from './template.service';
 import { Prisma, Template } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('template')
 export class TemplateController {
   constructor(
     private readonly templateService: TemplateService,
     private readonly jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
 
   @Get()
@@ -26,12 +28,42 @@ export class TemplateController {
     return this.templateService.getTemplate(id);
   }
 
-  @Post(':user_id')
-  createTemplate(
-    @Param('user_id') user_id,
-    @Body() data: Prisma.TemplateCreateInput,
-  ): Promise<Template> {
-    return this.templateService.createTemplate(user_id, data);
+  @Post()
+  createTemplate(@Req() request: Request): Promise<Template> {
+    const token = request.headers['authorization'].split(' ')[1];
+    const { id } = this.jwtService.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+    const data = {
+      items: [],
+      linkStyle: {
+        background: '#ffffff',
+        borderColor: '#DDDDDD',
+        borderWidth: '1px',
+        borderRadius: '5px',
+        hoverAnimation: 'fade',
+        fontSize: '1rem',
+        fontColor: '#7d7aff',
+        fontWeight: '600',
+      },
+      headerStyle: {
+        fontSize: '1.5rem',
+        fontColor: '#fa0000',
+        fontWeight: 'semibold',
+      },
+      titleStyle: {
+        fontSize: '2rem',
+        fontFamily: 'oblique',
+        fontColor: '#11ff00',
+        fontWeight: '600',
+      },
+      name: '',
+      background: '#ffffff',
+      photo:
+        'https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg',
+      title: '',
+    };
+    return this.templateService.createTemplate(id, data);
   }
 
   @Put(':id')
